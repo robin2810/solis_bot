552efdfa94c982309d9f1e62b6b83a2b50edc594
robin2810 <49187032+robin2810@users.noreply.github.com>
2020-09-16 21:37:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,6 +98,23 @@ bot.on('message', async function (user, userID, channelID, message, evt) {
 
     var serverID = evt.d.guild_id;
 
+    if (message.substring(0, 1) == '!') {
+      var args = message.substring(1).split(' ');
+      var cmd = args[0];
+      var cmd2 = args[1];
+
+      //args = args.splice(1);
+      switch(cmd) {
+        case 'apply':
+          bot.sendMessage({
+            to: channelID,
+            message: "https://forms.gle/4dNx1kgxzrYtqjxe7",
+            embed: out
+          });
+        break;
+      }
+    }
+
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '&') {
